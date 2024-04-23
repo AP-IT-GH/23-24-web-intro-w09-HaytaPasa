93870b680edbbd1ac37acdfc6bbd72718574f507
HaytaPasa <160629191+HaytaPasa@users.noreply.github.com>
2024-04-23 20:05:43 +0200

    Labo9/Oef1
    v1

--- oefening-01-02/js/mapScript.js
+++ oefening-01-02/js/mapScript.js
@@ -2,9 +2,14 @@
 * Gebruik hiervoor de documentatie op https://leafletjs.com/ 
 * Verander in de code online alle "var" in "let".
 */
+let map = L.map('apMap').setView([51.23009, 4.41616], 17);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(apMap);
 
-
+let marker = L.marker([51.23009, 4.41616]).addTo(apMap);
 
 
 
@@ -14,7 +19,8 @@
 
 
 // bepaal de rechthoek rondom het gebouw van AP
-
+let bounds = [[51.23041, 4.4155], [51.22991, 4.41675]];
 // kleur de rechthoek in met de rode AP-kleur
-
+L.rectangle(bounds, {color: "#97000C", weight: 1}).addTo(apMap);
 // plaats een marker met als tekst "AP-Hogeschool" en eronder "Ellermanstraat 33"
+marker.bindPopup("<b>AP-Hogeschool</b><br>Ellermanstraat 33").openPopup();
